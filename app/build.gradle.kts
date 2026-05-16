d8a29020b814dc153bd1820c93bd643322760d7d
Faizal Zain <faizalmzain@gmail.com>
2026-05-16 15:25:58 +0800

    Bump version to 1.2.0 (17)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.fmz.spenitaicore"
         minSdk = 31
         targetSdk = 35
-        versionCode = 16
-        versionName = "1.1.3"
+        versionCode = 17
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
