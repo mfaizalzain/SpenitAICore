d88a43e43611a439c08aec0303d67286a341f8ca
Faizal Zain <faizalmzain@gmail.com>
2026-05-21 14:29:08 +0800

    Bump version to 1.2.3 (20) for production release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.fmz.spenitaicore"
         minSdk = 31
         targetSdk = 35
-        versionCode = 19
-        versionName = "1.2.2"
+        versionCode = 20
+        versionName = "1.2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
