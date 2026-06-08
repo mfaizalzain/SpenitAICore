5af1f66b76f2a4e2f8315484156db4da3be44a5c
Faizal Zain <faizalmzain@gmail.com>
2026-06-09 00:02:19 +0800

    chore(release): bump version to 1.2.8 (25)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.fmz.spenitaicore"
         minSdk = 31
         targetSdk = 35
-        versionCode = 24
-        versionName = "1.2.7"
+        versionCode = 25
+        versionName = "1.2.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
