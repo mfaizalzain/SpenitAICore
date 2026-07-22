91808915851c7b08661be6e875e515f84f0d776b
Faizal Zain <faizalmzain@gmail.com>
2026-07-22 15:12:10 +0800

    chore(release): bump version to 1.2.9 (26)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.fmz.spenitaicore"
         minSdk = 31
         targetSdk = 36
-        versionCode = 25
-        versionName = "1.2.8"
+        versionCode = 26
+        versionName = "1.2.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
